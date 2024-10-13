8e824e9552bffcc6e1cda6ced5ab77d6c7647039
julius <nyerereoffice@gmail.com>
2024-10-13 05:09:46 +0300

    Contact Page

--- website/views.py
+++ website/views.py
@@ -3,3 +3,14 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html', {})
+
+
+def contact(request):
+    if request.method == "POST":
+        message_name = request.POST['message_name']
+        message_email = request.POST['message_email']
+        message = request.POST['message']
+        
+        return render(request, 'contact.html', {'message_name': message_name})
+    else:
+        return render(request, 'contact.html', {})
